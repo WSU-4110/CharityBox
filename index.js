1c1c271b7eea579e40a221d34e3f4f23831aeae7
go8329-nguyenevan <go8329@wayne.edu>
2020-03-26 16:17:46 -0400

    added login page and associated html

---
+++ index.js
@@ -0,0 +1,22 @@
+var firebaseConfig = 
+{
+    apiKey: "AIzaSyDXjH-Ou-kzGztLmYVOw0xtG0wzj9S-H3M",
+    authDomain: "chartiybox.firebaseapp.com",
+    databaseURL: "https://chartiybox.firebaseio.com",
+    projectId: "chartiybox",
+    storageBucket: "chartiybox.appspot.com",
+    messagingSenderId: "954464642263",
+    appId: "1:954464642263:web:d687e90346eefec322fc92",
+    measurementId: "G-W0T1VZEKY4"
+};
+  
+// Initialize Firebase
+  
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+$("#btn-login").click(function(){
+    
+    
+});
