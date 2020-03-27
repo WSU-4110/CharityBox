494d86e12063633fb72bd107f95362b9a92eef94
go8329-nguyenevan <go8329@wayne.edu>
2020-03-27 15:36:18 -0400

    login testpage and nodejs implementation of firebase auth

--- index.js
+++ index.js
@@ -15,8 +15,27 @@ var firebaseConfig =
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+firebase.auth.Auth.Persistence.LOCAL;
 
 $("#btn-login").click(function(){
     
-    
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    if(email != "" && password != ""){
+        var result = firebase.auth().signInWithEmailAndPassword(email, password);
+        
+        result.catch(function(error()
+        {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+            window.alert("Message: " + errorMessage);
+        });
+    }
+    else
+    {
+        window.alert("Please fill out all fields.");       
+    }
 });
